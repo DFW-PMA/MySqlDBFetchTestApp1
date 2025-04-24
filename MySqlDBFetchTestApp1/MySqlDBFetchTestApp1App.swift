0d3802b67642d53ab5e4e093fa5a429d78d99627
Daryl R Cox <dcox@justmacapps.net>
2025-04-24 11:50:38 -0500

    Initial Commit

---
+++ MySqlDBFetchTestApp1/MySqlDBFetchTestApp1App.swift
@@ -0,0 +1,17 @@
+//
+//  MySqlDBFetchTestApp1App.swift
+//  MySqlDBFetchTestApp1
+//
+//  Created by Daryl Cox on 4/24/25.
+//
+
+import SwiftUI
+
+@main
+struct MySqlDBFetchTestApp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MySqlDBFetchTestApp1Tests/MySqlDBFetchTestApp1Tests.swift
@@ -0,0 +1,16 @@
+//
+//  MySqlDBFetchTestApp1Tests.swift
+//  MySqlDBFetchTestApp1Tests
+//
+//  Created by Daryl Cox on 4/24/25.
+//
+
+import Testing
+
+struct MySqlDBFetchTestApp1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
